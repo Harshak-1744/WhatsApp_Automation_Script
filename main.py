5120c355812854055ad76d94de48a410b15acc93
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-08-13 08:38:48 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+# Packages
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from urllib.parse import quote
+import time
+
+# Config
+login_time = 30     # Time for login (in seconds)
+new_msg_time = 10    # TTime for a new message (in seconds)
+send_msg_time = 10   # Time for sending a message (in seconds)
+country_code = 91   # Set your country code
+
+# Create driver
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+# Encode Message Text
+with open('message.txt', 'r') as file:
+    msg = quote(file.read())
+
+# Open browser with default link
+link = 'https://web.whatsapp.com'
+driver.get(link)
+time.sleep(login_time)
+
+# Loop Through Numbers List
+with open('numbers.txt', 'r') as file:
+    for n in file.readlines():
+        num = n.rstrip()
+        link = f'https://web.whatsapp.com/send/?phone={country_code}{num}&text={msg}'
+        driver.get(link)
+        actions = ActionChains(driver)
+        time.sleep(5)
+        actions.send_keys(Keys.ENTER)
+        time.sleep(new_msg_time)
+        #actions.send_keys(Keys.ENTER)
+        actions.perform()
+        time.sleep(send_msg_time)
+
+# Quit the driver
+driver.quit()
